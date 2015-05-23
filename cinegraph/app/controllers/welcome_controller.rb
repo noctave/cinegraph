5b2ed12b3d0062df68b4fe4d01e214dca06dfa01
Vincent Pedussel <vincent.pedussel@gmail.com>
2015-05-23 17:05:43 +0200

    Add function search with array on page accueil, run ok

--- cinegraph/app/controllers/welcome_controller.rb
+++ cinegraph/app/controllers/welcome_controller.rb
@@ -1,6 +1,11 @@
 class WelcomeController < ApplicationController
   def accueil
   	@movies = Movie.all
-  	@tests = Imdb::Search.new("Iron man").movies
+  	#@tests = Imdb::Search.new("Iron man").movies
+  	if params[:q]
+      @tests = Imdb::Search.new(params[:q]).movies
+    else
+	  @tests = nil
+    end
   end
 end
