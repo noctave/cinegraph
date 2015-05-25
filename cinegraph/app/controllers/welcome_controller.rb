bd750d7872fea1381b8251a792def0e301bb5fad
alexandremachado <alexandre.machadodossantos@y-nov.com>
2015-05-25 14:00:02 +0200

    ajouts des liens + movies

--- cinegraph/app/controllers/welcome_controller.rb
+++ cinegraph/app/controllers/welcome_controller.rb
@@ -15,7 +15,23 @@ class WelcomeController < ApplicationController
   		puts imdbMovie.id
   		puts imdbMovie.url
   		puts current_user.id
-  		movie.Create(imdbMovie.id,imdbMovie.title,imdbMovie.url,current_user.id)
+
+  		#Movie::create(imdbMovie.id,imdbMovie.title,imdbMovie.url,current_user.id)
+      #params = params.require(:movie).permit(:movie_id, :title, :url, :user_id)
+      #arg.id = imdbMovie.id
+      #arg.title = imdbMovie.title
+      #arg.url = imdbMovie.url
+      #arg.id = current_user.id
+      #Movie::create(arg)
+
+      arg = {:movie_id => imdbMovie.id, :title => imdbMovie.title, :url => imdbMovie.url, :user_id => current_user.id}
+      #, "controller"=>"welcome", "action"=>"accueil"
+
+      puts arg.inspect
+      Movie::create(arg)
+
+
+
   		redirect_to (root_path)
   	end
   end
