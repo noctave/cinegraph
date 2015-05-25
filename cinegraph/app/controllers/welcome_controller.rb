e23a9bd0b21c6a4299e6594ff690ea7ee4a67bda
Vincent Pedussel <vincent.pedussel@gmail.com>
2015-05-25 14:17:10 +0200

    File modif + index_movie fonctionne en fonction de l user actuel connect

--- cinegraph/app/controllers/welcome_controller.rb
+++ cinegraph/app/controllers/welcome_controller.rb
@@ -11,27 +11,8 @@ class WelcomeController < ApplicationController
 	if params[:v]
   		id = params[:v]
   		imdbMovie = Imdb::Movie.new(id)
-  		puts imdbMovie.title
-  		puts imdbMovie.id
-  		puts imdbMovie.url
-  		puts current_user.id
-
-  		#Movie::create(imdbMovie.id,imdbMovie.title,imdbMovie.url,current_user.id)
-      #params = params.require(:movie).permit(:movie_id, :title, :url, :user_id)
-      #arg.id = imdbMovie.id
-      #arg.title = imdbMovie.title
-      #arg.url = imdbMovie.url
-      #arg.id = current_user.id
-      #Movie::create(arg)
-
-      arg = {:movie_id => imdbMovie.id, :title => imdbMovie.title, :url => imdbMovie.url, :user_id => current_user.id}
-      #, "controller"=>"welcome", "action"=>"accueil"
-
-      puts arg.inspect
-      Movie::create(arg)
-
-
-
+      	arg = {:movie_id => imdbMovie.id, :title => imdbMovie.title, :url => imdbMovie.url, :user_id => current_user.id}
+      	Movie::create(arg)
   		redirect_to (root_path)
   	end
   end
